23ca6ec5b2896f0350719d487eed779a9d220ede
Nick Rupert <nickrupert7@gmail.com>
2021-10-19 12:47:13 -0400

    Implemented tests for TEDayOfMonth

--- tests/TestCases/TEDayOfMonthTest.php
+++ tests/TestCases/TEDayOfMonthTest.php
@@ -9,19 +9,81 @@ use Numenor\FowlerRecurringEvents\TemporalExpressions\TEDayOfMonth;
 
 class TEDayOfMonthTest extends TestCase
 {
-    public function testIncorrectDateReturnsFalse() {
-        $startOfMonth = new TEDayOfMonth(1);
-        $testDate = new Carbon('December 25, 2021');
+    public function testBasicIncorrectDateReturnsFalse()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1);
+        $testDate = new Carbon('2021-12-25');
 
         $result = $startOfMonth->includes($testDate);
 
         $this->assertFalse($result);
     }
 
-    public function testCorrectDateReturnsTrue() {
+    public function testBasicCorrectDateReturnsTrue()
+    {
 
-        $startOfMonth = new TEDayOfMonth(1);
-        $testDate = new Carbon('December 1, 2021');
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1);
+        $testDate = new Carbon('2021-12-01');
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCorrectDateWithIncorrectFrequencyReturnsFalse()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1, 2);
+        $testDate = new Carbon('2021-12-01');
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertFalse($result);
+    }
+
+    public function testCorrectDateWithCorrectFrequencyReturnsTrue()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1, 2);
+        $testDate = new Carbon('2021-11-01');
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCorrectDateFromEndWithIncorrectFrequencyReturnsFalse()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), -1, 2);
+        $testDate = new Carbon('2021-12-31');
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertFalse($result);
+    }
+
+    public function testCorrectDateFromEndWithCorrectFrequencyReturnsTrue()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), -1, 2);
+        $testDate = new Carbon('2021-11-30');
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCorrectDateAcrossYearsWithIncorrectFrequencyReturnsFalse()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1, 5);
+        $testDate = new Carbon('2022-01-01'); //15 months later
+
+        $result = $startOfMonth->includes($testDate);
+
+        $this->assertFalse($result);
+    }
+
+    public function testCorrectDateAcrossYearsWithCorrectFrequencyReturnsTrue()
+    {
+        $startOfMonth = new TEDayOfMonth(new Carbon('2021-01-01'), 1, 5);
+        $testDate = new Carbon('2022-04-01'); //15 months later
 
         $result = $startOfMonth->includes($testDate);
 
